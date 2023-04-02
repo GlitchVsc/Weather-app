a928929fba7b02da81579e11a4dc61b2aca603bd
GlitchVsc <tolakosb312@gmail.com>
2023-04-02 14:06:43 +0300

    transforming the API data

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ import './style.css'
 import {getWeather} from "./weather"
 
 getWeather(10,10, Intl.DateTimeFormat().resolvedOptions().timeZone).then(
-    res => {
-        console.log(res.data)
+    data => {
+        console.log(data)
     }
 )
 
